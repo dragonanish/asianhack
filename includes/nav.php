0da1939f1a35d31ba22eca6710b58d48cd9e5fbd
AlkaShilpakar <alshu.shilp@gmail.com>
2019-08-24 11:27:54 +0545

    nav

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,7 @@
 <div class="container-fluid" style="background-color: #11748B">
     <div class="row">
         <div class="col-3">
-
+            <img src="../asset/images/loho.png" alt="" height="80px" width="300px" >
         </div>
         <div class="col-1"></div>
         <div class="col-8">
@@ -30,7 +30,7 @@
                             <a class="nav-link pr-3 font-weight-bold"  style="font-size: 20px; " href="../view/about.php" name="about">About</a>
                         </li>
                         <li class="nav-item pt-1">
-                            <a type="button" class="btn btn-danger btn-md text-light justify-content-center" href="../view/donate.php" name="login">DONATE</a>
+                            <a type="button" class="btn btn-danger btn-md text-light justify-content-center" href="../view/donate.php" name="donate">DONATE</a>
                         </li>
                         <li class="nav-item pl-2 pt-1">
                             <a type="button" class="btn btn-outline-dark btn-md text-light justify-content-center" href="../view/login.php" name="login">LOGIN</a>
