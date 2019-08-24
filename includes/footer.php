6d1ad5a9907e772bcdd2d7bba253f11bf77fc569
AlkaShilpakar <alshu.shilp@gmail.com>
2019-08-24 14:30:52 +0545

    footer change

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,9 @@
 <div class="container-fluid" style="background-color: #11748B">
     <div class="row mt-4">
         <div class="col-12">
-            <a href="#" class="p-2 text-dark border-right">Contact us</a>
-            <a href="#" class="p-2 text-dark">Terms of service</a>
-            <span class=" text-dark p-1 float-right">2018 ArtMela All rights reserved</span>
+            <a href="#" class="p-2 text-light border-right">Contact us</a>
+            <a href="#" class="p-2 text-light">Terms of service</a>
+            <span class=" text-light p-1 float-right">2018 SahayogiHaat All rights reserved</span>
         </div>
     </div>
 </div>
--- view/donate.php
+++ view/donate.php
@@ -19,22 +19,22 @@ include '../includes/nav.php';
 
 <div class="container-fluid pt-2">
     <div class="row">
-        <div class="col-3"></div>
+        <div class="col-2"></div>
         <div class="col-7 pt-2">
             
-            <div class="pt-4 pl-3">
-                <!-- Card  -->
+            <div class="pt-4">
+
                 <div class="card shadow-sm">
-                    <!-- Card content -->
+
                     <div class="card-body">
 
                         <h2 class="card-title text-danger">Support Us</h2>
                         <hr>
                         <!-- Text -->
                         <p class="text-muted">You can support us by direct donating</p>
-                        <p class="card-text"><h3>1. Nabil bank</h3><p class="pl-5">Account name: sahayogiHaat <br>Account name:1410017504959</p>
-                        <p class="card-text"><h3>2. Nepal Investment bank</h3><p class="pl-5">Account name: sahayogiHaat<br>Account name:1410017504959</p>
-                        <p class="card-text"><h3>3. Prabhu bank</h3><p class="pl-5">Account name: sahayogiHaat<br>Account name:1410017504959</p>
+                        <p class="card-text"><h3>1. Nabil bank</h3><p class="pl-5">Account name: sahayogiHaat <br>Account number:1410017504959</p>
+                        <p class="card-text"><h3>2. Nepal Investment bank</h3><p class="pl-5">Account name: sahayogiHaat<br>Account number:1410017504959</p>
+                        <p class="card-text"><h3>3. Prabhu bank</h3><p class="pl-5">Account name: sahayogiHaat<br>Account number:1410017504959</p>
 
 
                        
