0012c34a6ac60107e99e99c88169d63b6842147f
dibaspaudel <46590839+dibaspaudel@users.noreply.github.com>
2019-08-24 14:27:44 +0545

    post

--- action/act_post.php
+++ action/act_post.php
@@ -1,58 +1,81 @@
 <?php
-include("DbConnection.php")
-if (isset($_POST['btn_submit'])) {
+session_start();
+include("DbConnection.php");
+$msg = "";
+if(isset($_POST['btn_submit'])) {
+
     $post=$_POST['report'];
-    $Postimage=$_POST['image'];
     $casetype=$_POST['case'];
     $location=$_POST['location'];
-    $FullName=$_POST['name1'];
-    $FullNameOPT=$_POST['name2'];
-    $address1=$_POST['address1'];
-    $address2=$_POST['address2'];
-    $Phone1=$_POST['num1'];
-    $Phone2=$_POST['num2'];
-    $Email=$_POST['email'];
 
+    $image = $_FILES['image']['name'];
+    $image_temp = $_FILES['image']['tmp_name'];
+    $ext = pathinfo($image,PATHINFO_EXTENSION);
+    $image = md5($image);
+    $image = $image . "." . $ext;
+    $target = "../image/".$image;
 
-    if($_SESSION['UserId']==null){
-        if($post!=null && $casetype!=null && $location!=null && $FullName!=null&& $address1!=null && $Phone1!=null){
-          $userdetail_query="insert into user(FullName,FullNameOPT,Email,Phone1,Phone2, Address1, Address2, IsEmergency)".
-            "values('$FullName','$FullNameOPT','$Email','$Phone1','$Phone2','$address1','$address2','1')";
+    // echo $_SESSION["UserId"];
+    // die();
+    // if($_SESSION['UserId']==null){
+    //     if($post!=null && $casetype!=null && $location!=null && $FullName!=null&& $address1!=null && $Phone1!=null){
+    //       $userdetail_query="insert into user(FullName,FullNameOPT,Email,Phone1,Phone2, Address1, Address2, IsEmergency)".
+    //         "values('$FullName','$FullNameOPT','$Email','$Phone1','$Phone2','$address1','$address2','1')";
+    //     }
+    //     else{
+    //         header("location:../view/report.php");
+    //     }
+
+    //     $userdetil_result= mysqli_query($con, $userdetail_query);
+
+    //     $retriveLastUser = "select UserId from user order by UserId desc Limit 1";
+    //     $retriveLastUserResults= mysqli_query($db,$retriveLastUser);
+
+    //     if(mysqli_num_row($retriveLastUser) == 1){
+    //         $retriveLastUserData = mysqli_fetch_assoc($retriveLastUserResults);
+    //         $UserId = $retriveLastUserData['UserId'];
+
+    //         $post_query="insert into post(Post,Postimage,casetype,location,UserId)".
+    //         "values('$Post','$image','$casetype','$location','$UserId')";
+
+    //         $userdetil_result= mysqli_query($con, $userdetail_query);
+    //         header("location:../view/home.php");
+    //     }
+    // }else{
+    //     if($post!=null && $casetype!=null && $location!=null && $FullName!=null&& $address1!=null && $Phone1!=null){
+    //         $UserId = $_SESSION['UserId'];
+    //         $post_query="insert into post(Post,Postimage,casetype,location,UserId)".
+    //         "values('$Post','$Postimage','$casetype','$location','$UserId')";
+
+    //         $userdetil_result = mysqli_query($db,$post_query);
+    //     }
+    //     else{
+    //         header("location:../view/report.php");
+    //     }
+    // }
+
+    if($_SESSION["UserId"] != null){
+        if($post!=null && $casetype!=null && $location!=null){
+            $UserId = $_SESSION['UserId'];
+            $post_query="insert into post(Post,Postimage,casetype,location,UserId)".
+            "values('$post','$image','$casetype','$location','$UserId')";
+
+            $userdetil_result = mysqli_query($db,$post_query);
+
+            if(move_uploaded_file($image_temp,$target))
+            {
+                $msg ="Image uploaded successfully";
+            }
+            else
+            {
+                $msg = "There was a problem uploading image";
+            }
+            header("location:../view/home.php");
         }
         else{
-            header("location:../view/report.php");
-        }
-
-        $userdetil_result= mysqli_query($con, $userdetail_query);
-
-        $retriveLastUser = "select UserId from user order by UserId desc Limit 1";
-        $retriveLastUserResults= mysqli_query($db,$retriveLastUser);
-
-        if(mysqli_num_row($retriveLastUser) == 1){
-            $retriveLastUserData = mysqli_fetch_assoc($retriveLastUserResults);
-            $UserId = $retriveLastUserData['UserId'];
-
-            $post_query="insert into post(Post,Postimage,casetype,location)".
-            "values('$Post','$Postimage','$casetype','$location')";
-
-        }
-    }else{
-        if($post!=null && $casetype!=null && $location!=null && $FullName!=null&& $address1!=null && $Phone1!=null){
-        $post_query="insert into post(Post,Postimage,casetype,location,)".
-            "values('$Post','$Postimage','$casetype','$location')";
-
-        }
-        else{
-            header("location:../view/report.php");
+            header("location:../view/post.php");
         }
     }
-
-    //$post_result= mysqli_query($con, $post_query);
-
-     $userdetil_result= mysqli_query($con, $userdetail_query);
-
-     $retriveLastUser
-
     
 }
 ?>
